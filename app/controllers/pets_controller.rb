ba70c25053509814bb0428ad43b13b8811333686
Wilson Wang <wilsonw926@gmail.com>
2017-11-08 17:38:51 -0800

    integrated simple form/pet db needs updating

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,1 +1,14 @@
 class PetsController < ApplicationController
+
+	def create
+		pet = Pet.new
+		pet.update(params.require(:pet).permit(:name))
+ 	    if pet.save
+ 			redirect_to trainer_path(id: current_trainer.id)
+ 		else
+ 			flash[:error] = @new_pet.errors.full_messages.to_sentence
+ 			redirect_to new_pet_path
+ 		end
+	end
+
+end
---
+++ app/controllers/shelters_controller.rb
@@ -0,0 +1,1 @@
+class SheltersController < ApplicationController
