70d266e7207d70af1718f30a4c40abdc3073cba2
nitinsampath <nitfremont@gmail.com>
2017-11-13 16:33:12 -0800

    added favorites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108012039) do
+ActiveRecord::Schema.define(version: 20171114003045) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pets", force: :cascade do |t|
     t.datetime "created_at", null: false
