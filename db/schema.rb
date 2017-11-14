ad1e5c35cad2722f180c2aee705c0cdb3a1e055e
nitinsampath <nitfremont@gmail.com>
2017-11-13 17:07:49 -0800

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114003045) do
+ActiveRecord::Schema.define(version: 20171114010337) do
 
   create_table "favorites", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20171114003045) do
   create_table "pets", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "type"
+    t.string "activity_level"
+    t.string "personality"
+    t.string "size"
   end
 
   create_table "shelters", force: :cascade do |t|
