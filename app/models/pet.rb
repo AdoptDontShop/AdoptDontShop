9febd196d64f21d04833f64473542d8820521cc6
nitinsampath <nitfremont@gmail.com>
2017-11-13 16:45:58 -0800

    saved

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,5 +2,5 @@ class Pet < ApplicationRecord
 	belongs_to :shelter, optional:true
 	belongs_to :user, optional:true
 	validates :name, uniqueness: true, length: {minimum: 1}
-	has_many :favorites, optional:true
+	has_many :favorites
 end
