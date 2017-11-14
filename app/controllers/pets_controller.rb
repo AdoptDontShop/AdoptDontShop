ee25bf9dbdc6f8a926b29a8ef149e9154068daf3
nitinsampath <nitfremont@gmail.com>
2017-11-13 17:32:07 -0800

    adding pets works!

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,9 +2,10 @@ class PetsController < ApplicationController
 
 	def create
 		pet = Pet.new
-		pet.update(params.require(:pet).permit(:name))
+		pet.update(params.require(:pets).permit(:name))
+
  	    if pet.save
- 			redirect_to trainer_path(id: current_trainer.id)
+ 			redirect_to new_pet_path(id: pet.id)
  		else
  			flash[:error] = @new_pet.errors.full_messages.to_sentence
  			redirect_to new_pet_path
