9f3acb88899638b570febe2ba848ab19a8c65960
Selina Feng <selina.feng@berkeley.edu>
2017-11-15 14:47:05 -0800

    enabled image upload and rendering

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,5 +3,9 @@ class Pet < ApplicationRecord
 	belongs_to :user, optional:true
 	validates :name, uniqueness: true, length: {minimum: 1}
 	has_many :favorites
-	has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+	has_attached_file :image, 
+		:path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+		:url => "/system/:attachment/:id/:style/:filename",
+		:styles => { small: "64x64", med: "100x100", large: "200x200" },
+		:default_url => "dog.jpg"
 end
