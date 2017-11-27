1238fa394a18c24cb9af1c7384ccdcc008ea1091
Wilson Wang <wilsonw926@gmail.com>
2017-11-27 10:37:53 -0800

    1 pet 1 shelter seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pet.create name: "Bailey", species: "Dog", activity_level: "Medium", personality: "Friendly", size: "Big"
+Shelter.create email: "shelter@gmail.com", password: 'password'
