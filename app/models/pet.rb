b8b3efcf3234f15936c4610366e5ee56bae7c3ca
Xin Yi Chen <xinyichen516@gmail.com>
2017-11-27 22:52:15 -0800

    styling done

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,13 +3,13 @@ class Pet < ApplicationRecord
 	belongs_to :user, optional:true
 	validates :name, uniqueness: true, length: {minimum: 1}
 	has_many :favorites
-	has_attached_file :image, 
+	has_attached_file :image,
 		:path => ":rails_root/public/system/:attachment/:id/:style/:filename",
 		:url => "/system/:attachment/:id/:style/:filename",
-		:styles => { small: "64x64", med: "100x100", large: "200x200" },
+		:styles => { small: "64x64", med: "100x100", large: "200x200", extralarge: "400x400" },
 		:default_url => "dog.jpg"
 
-	validates_attachment_content_type :image, 
+	validates_attachment_content_type :image,
 		:content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 		#:matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
 
